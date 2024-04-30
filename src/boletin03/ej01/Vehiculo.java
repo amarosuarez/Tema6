ac860148b5d1f67ac8af2c2c6ede77f4978600fb
asuarez <asuarez@106-27.info2>
2024-04-30 13:03:02 +0200

    Clase vehiculo terminada

--- src/boletin03/ej01/Vehiculo.java
+++ src/boletin03/ej01/Vehiculo.java
@@ -21,7 +21,7 @@ public class Vehiculo {
 	 * Atributo que almacena el color del vehículo
 	 */
 	protected String color = "";
-	
+
 	/**
 	 * Atributo que almacena la matrícula del vehículo
 	 */
@@ -45,8 +45,17 @@ public class Vehiculo {
 	/**
 	 * Constructor vacío
 	 */
-	public Vehiculo() {}
+	public Vehiculo() {
+	}
 
+	/**
+	 * Constructor con los parámetros marca, modelo, color y matricula
+	 * 
+	 * @param marca     Marca del coche
+	 * @param modelo    Modelo del coche
+	 * @param color     Color del coche
+	 * @param matricula Matricula del coche
+	 */
 	public Vehiculo(String marca, String modelo, String color, String matricula) {
 		// Comprobamos que la marca no sea nulo ni vacío
 		if (marca != null && !marca.equals("")) {
@@ -67,6 +76,87 @@ public class Vehiculo {
 		if (matricula != null && !matricula.equals("")) {
 			this.matricula = matricula;
 		}
+	}
+
+	/**
+	 * Método que para el vehículo
+	 */
+	public void parar() {
+		// Cambiamos la velocidad a 0
+		this.velocidad = 0;
+	}
+
+	/**
+	 * Método que enciende el motor
+	 */
+	public void arrancar() {
+		// Encendemos el motor
+		this.motor = true;
+	}
+
+	/**
+	 * Método que acelera el vehículo
+	 * 
+	 * @param velocidad Velocidad que se le añade al vehículo
+	 */
+	public void acelerar(double velocidad) {
+		// Aumentamos la velocidad
+		this.velocidad += velocidad;
+	}
+
+	/**
+	 * Método que frena el vehículo
+	 * 
+	 * @param velocidad Velocidad que se le resta al vehículo
+	 */
+	public void frenar(double velocidad) {
+		// Bajamos la velocidad
+		if (this.velocidad > velocidad) {
+			this.velocidad -= velocidad;
+		} else {
+			this.velocidad = 0;
+		}
+	}
+
+	/**
+	 * Método que sube una marcha del vehículo
+	 */
+	private void subirMarcha() {
+		// Aumentamos la marcha del vehículo
+		if (this.marcha + 1 <= 6) {
+			this.marcha++;
+		}
+	}
+
+	/**
+	 * Método que baja la marcha del vehículo
+	 */
+	private void bajarMarcha() {
+		// Bajamos la marcha del vehículo
+		if (this.marcha - 1 >= 0) {
+			this.marcha--;
+		}
+	}
+
+	/**
+	 * Método que almacena la marcha del vehículo
+	 * 
+	 * @param marcha Marcha del vehículo a almacenar
+	 */
+	public void setMarcha(int marcha) {
+		// Comprobamos que la marcha esté entre 1 y 6
+		if (marcha >= 1 && marcha <= 6) {
+			this.marcha = marcha;
+		}
+	}
+	
+	/**
+	 * Método que almacena el estado del motor del vehículo
+	 * @param motor Estado del motor a almacenar
+	 */
+	public void setMotor(boolean motor) {
+		// Almacenamos el valor
+		this.motor = motor;
 	}
 
 }
